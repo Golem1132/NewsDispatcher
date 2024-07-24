a8cb62983e3241c5ec254b333b5a9963469664bf
Adam Hałas <adam.halas1132@gmail.com>
2024-07-24 21:32:46 +0200

    Style WelcomScreenBottomBar

--- app/src/main/java/com/example/newsdispatcher/bottombar/WelcomeScreenBottomBar.kt
+++ app/src/main/java/com/example/newsdispatcher/bottombar/WelcomeScreenBottomBar.kt
@@ -4,27 +4,36 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 
 @Composable
 fun WelcomeScreenBottomBar(
     onSkip: () -> Unit,
     onNext: () -> Unit,
 ) {
-    BottomAppBar() {
+    BottomAppBar(containerColor = Color.Transparent) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
-                modifier = Modifier.clickable (onClick = onSkip),
-                text = "Skip")
+                modifier = Modifier.clickable(onClick = onSkip),
+                text = "Skip", fontSize = 5.em, fontWeight = FontWeight.Bold
+            )
             Text(
-                modifier = Modifier.clickable (onClick = onNext),
-                text = "Next")
+                modifier = Modifier.clickable(onClick = onNext),
+                text = "Next", fontSize = 5.em, fontWeight = FontWeight.Bold
+            )
         }
     }
 }
